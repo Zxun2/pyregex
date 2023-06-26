bca04a676e47beefa46a9a22ff73532e65ef9906
Zong Xun <zxun2000@gmail.com>
2023-06-26 20:56:42 +0800

    test: add unit tests

---
+++ src/test_tokens.py
@@ -0,0 +1,32 @@
+from tokens import *
+
+
+def test_Asterisk():
+    assert issubclass(Asterisk, ZeroOrMore)
+
+    a = Asterisk()
+    assert a is not None
+
+    assert type(a) == Asterisk
+
+
+def test_NotToken():
+    assert issubclass(NotToken, Token) == True
+
+    nt = NotToken(char='^')
+    assert nt is not None
+    assert nt.char == '^'
+
+
+def test_Bracket():
+    br = Bracket()
+    assert br is not None
+    br = LeftBracket()
+    assert br is not None
+    br = RightBracket()
+    assert br is not None
+
+
+def test_Escape():
+    escape = Escape()
+    assert escape is not None
